b5100ba720d61767861473e2a68bd6ff7526ee04
AgostaSalvatore <agosta.boolean@gmail.com>
2025-04-16 13:49:31 +0200

    Add /bacheca endpoint with title,content,image and tags as array

--- app.js
+++ app.js
@@ -7,12 +7,47 @@ const app = express();
 //definisco la porta
 const port = 3000;
 
-//creo la rotta /
+//creo la rotta "/" home
 app.get("/", (req,res) =>{
     res.send("Server del mio Blog");
 });
 
+app.get("/bacheca", (req,res) =>{
+    const posts = [
+        {
+            title: "Post 1",
+            content: "Contenuto del post 1",
+            image : "imgs/post1.jpg",
+            tags: ["tag1", "tag2", "tag3"]
+        },
+        {
+            title: "Post 2",
+            content: "Contenuto del post 2",
+            image : "imgs/post2.jpg",
+            tags: ["tag1", "tag2", "tag3"]
+        },
+        {
+            title: "Post 3",
+            content: "Contenuto del post 3",
+            image : "imgs/post3.jpg",
+            tags: ["tag1", "tag2", "tag3"]
+        },
+        {
+            title: "Post 4",
+            content: "Contenuto del post 4",
+            image : "imgs/post4.jpg",
+            tags: ["tag1", "tag2", "tag3"]
+        },
+        {
+            title: "Post 5",
+            content: "Contenuto del post 5",
+            image : "imgs/post5.jpg",
+            tags: ["tag1", "tag2", "tag3"]
+        }
+    ];
 
+    res.json(posts);
+});
 
 
 //server in ascolto sulla porta 3000
