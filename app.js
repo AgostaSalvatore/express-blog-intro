9d1ea5912c8d7b64b570581b714ad420dd3f600b
AgostaSalvatore <agosta.boolean@gmail.com>
2025-04-16 14:14:42 +0200

    [FEAT/UPD] created postBlog.js and exported on app.js

--- app.js
+++ app.js
@@ -16,39 +16,6 @@ app.get("/", (req,res) =>{
 });
 
 app.get("/bacheca", (req,res) =>{
-    const posts = [
-        {
-            title: "Ciambellone",
-            content: "Ciambellone",
-            image : "<img src=./images/ciambellone.jpeg>",
-            tags: ["tag1", "tag2", "tag3"]
-        },
-        {
-            title: "Cracker Barbabietola",
-            content: "Cracker Barbabietola",
-            image : "<img src=./images/cracker_barbabietola.jpeg>",
-            tags: ["tag1", "tag2", "tag3"]
-        },
-        {
-            title: "Pane Fritto Dolce",
-            content: "Pane Fritto Dolce",
-            image : "<img src=./images/pane_fritto_dolce.jpeg>",
-            tags: ["tag1", "tag2", "tag3"]
-        },
-        {
-            title: "Pasta Barbabietola",
-            content: "Pasta Barbabietola",
-            image : "<img src=./images/pasta_barbabietola.jpeg>",
-            tags: ["tag1", "tag2", "tag3"]
-        },
-        {
-            title: "Torta Paesana",
-            content: "Torta Paesana",
-            image : "<img src=./images/torta_paesana.jpeg>",
-            tags: ["tag1", "tag2", "tag3"]
-        }
-    ];
-
     res.json(posts);
 });
 
@@ -57,3 +24,6 @@ app.get("/bacheca", (req,res) =>{
 app.listen(port, () => {
     console.log(`Server in ascolto sulla porta ${port}`);
 });
+
+//richiamo l'array dalla pagina postBlog.js
+const posts = require("./postBlog.js");
