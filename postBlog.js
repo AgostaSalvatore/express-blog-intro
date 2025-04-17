1014d40511db8db03fdfe006fb4f661b6d743e78
AgostaSalvatore <agosta.boolean@gmail.com>
2025-04-17 10:09:36 +0200

    [FIX] fix image link on postBlog.js

--- postBlog.js
+++ postBlog.js
@@ -2,31 +2,31 @@ const posts = [
     {
         title: "Ciambellone",
         content: "Ciambellone",
-        image : "<img src=./images/ciambellone.jpeg>",
+        image : "/images/ciambellone.jpeg",
         tags: ["tag1", "tag2", "tag3"]
     },
     {
         title: "Cracker Barbabietola",
         content: "Cracker Barbabietola",
-        image : "<img src=./images/cracker_barbabietola.jpeg>",
+        image : "/images/cracker_barbabietola.jpeg",
         tags: ["tag1", "tag2", "tag3"]
     },
     {
         title: "Pane Fritto Dolce",
         content: "Pane Fritto Dolce",
-        image : "<img src=./images/pane_fritto_dolce.jpeg>",
+        image : "/images/pane_fritto_dolce.jpeg",
         tags: ["tag1", "tag2", "tag3"]
     },
     {
         title: "Pasta Barbabietola",
         content: "Pasta Barbabietola",
-        image : "<img src=./images/pasta_barbabietola.jpeg>",
+        image : "/images/pasta_barbabietola.jpeg",
         tags: ["tag1", "tag2", "tag3"]
     },
     {
         title: "Torta Paesana",
         content: "Torta Paesana",
-        image : "<img src=./images/torta_paesana.jpeg>",
+        image : "/images/torta_paesana.jpeg",
         tags: ["tag1", "tag2", "tag3"]
     }
 ];
